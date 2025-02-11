ec205b0514daf0b5a15ed6202658775c2a4e4dff
Sérgio Adriano <sergioadrianomc@gmail.com>
2025-02-11 11:32:13 -0300

    requiriments.txt e correção da view.py e urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# urls.py (no diretório do projeto)
+
 from django.contrib import admin
 from django.urls import path
+from mysite.blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('minha-rota/', views.minha_view),
 ]
